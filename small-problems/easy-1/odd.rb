c64bead20cb0ed5a88d1bb36cdb46a3e0144c375
DavindraS <razrsharp25@gmail.com>
2018-02-27 21:17:39 -0500

    Rewrote method using remainder instead of modulus
    The Numeric#remainder method would return negative if number was
    negative so to fix that I used the 'abs' method to get the
    absolute value first.

--- small-problems/easy-1/odd.rb
+++ small-problems/easy-1/odd.rb
@@ -1,5 +1,22 @@
 # return true if absolute value of input is odd
 
 def is_odd?(number)
-  
+  number.abs.remainder(2) == 1
+end
+
+
+puts is_odd?(2)    # => false
+puts is_odd?(5)    # => true
+puts is_odd?(-17)  # => true
+puts is_odd?(-8)   # => false
+puts is_odd?(0)    # => false
+puts is_odd?(7)    # => true
+
+# first solution - kinda verbose
+def is_odd?(number)
+  if number.abs % 2 == 0
+    return false
+  else
+    return true
+  end
 end
