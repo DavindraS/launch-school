49716530860593ff68280bb40517282f88a762fa
DavindraS <razrsharp25@gmail.com>
2018-02-28 23:09:41 -0500

    Join all method calls onto one line

--- small_problems/easy_1/reverse_it_two.rb
+++ small_problems/easy_1/reverse_it_two.rb
@@ -1,10 +1,7 @@
 def reverse_words(input)
   # if the word is 5 or more chars, reverse in place
   # else leave word and move along
-  words = input.split
-  words.each{|word| word.reverse! if word.length >= 5}
-
-  return words.join(" ")
+  input.split.each{|word| word.reverse! if word.length >= 5}.join(" ")
 
 end
 
