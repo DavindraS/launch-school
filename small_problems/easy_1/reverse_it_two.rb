9ec1a8ac026257193d7b8ee72dc030754041d687
DavindraS <razrsharp25@gmail.com>
2018-02-28 23:05:01 -0500

    Add solution to Reverse It (Part 2) exercise

---
+++ small_problems/easy_1/reverse_it_two.rb
@@ -0,0 +1,13 @@
+def reverse_words(input)
+  # if the word is 5 or more chars, reverse in place
+  # else leave word and move along
+  words = input.split
+  words.each{|word| word.reverse! if word.length >= 5}
+
+  return words.join(" ")
+
+end
+
+puts reverse_words('Professional')          # => lanoisseforP
+puts reverse_words('Walk around the block') # => Walk dnuora the kcolb
+puts reverse_words('Launch School')         # => hcnuaL loohcS
