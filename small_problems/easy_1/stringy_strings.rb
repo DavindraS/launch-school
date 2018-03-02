05cc9b02dc996033ad12d45ff7c926da932208d8
DavindraS <razrsharp25@gmail.com>
2018-03-02 13:49:47 -0500

    Add further exploration solution

--- small_problems/easy_1/stringy_strings.rb
+++ small_problems/easy_1/stringy_strings.rb
@@ -1,15 +1,21 @@
-def stringy(num)
+def stringy(num, arg)
   count = Array.new(num) # array that will hold 'num' values
   count.each_index do |index| # for each index
-    index.even? ? count[index] = '1' : count[index] = '0' # write 1's and 0's
+    arg == 0 ? (index.even? ? count[index] = 0 : count[index] = 1) :
+    (index.even? ? count[index] = 1 : count[index] = 0) # write 1's and 0's
   end
   count.join() # Array#join to create the final string
 end
 
-puts stringy(6) == '101010'
-puts stringy(9) == '101010101'
-puts stringy(4) == '1010'
-puts stringy(7) == '1010101'
+puts stringy(6, 1) == '101010'
+puts stringy(9, 1) == '101010101'
+puts stringy(4, 1) == '1010'
+puts stringy(7, 1) == '1010101'
+
+puts stringy(6, 0) == '010101'
+puts stringy(9, 0) == '010101010'
+puts stringy(4, 0) == '0101'
+puts stringy(7, 0) == '0101010'
 
 =begin original solution
 def stringy(num)
@@ -23,5 +29,14 @@ def stringy(num)
     count -= 1
   end
   output_str
+end
+
+second solution similar to official solution
+def stringy(num)
+  count = Array.new(num) # array that will hold 'num' values
+  count.each_index do |index| # for each index
+    index.even? ? count[index] = '1' : count[index] = '0' # write 1's and 0's
+  end
+  count.join() # Array#join to create the final string
 end
 =end
