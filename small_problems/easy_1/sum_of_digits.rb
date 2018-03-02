71e95416731d3b3e9c1caf8f51e69bfefc189520
DavindraS <razrsharp25@gmail.com>
2018-03-02 15:16:15 -0500

    Add explanation of the steps for this solution

--- small_problems/easy_1/sum_of_digits.rb
+++ small_problems/easy_1/sum_of_digits.rb
@@ -1,5 +1,10 @@
 def sum(input)
+  p input.to_s.chars
   input.to_s.chars.map(&:to_i).inject(:+)
+  # 1. Take the input and convert it to a string 23 -> "23"
+  # 2. Call String#chars which results in an array of each character
+  # 3. We have an array which we then run map to_i on each index
+  # 4. Then we call inject with :+ symbol to add it all together
 end
 
 puts sum(23) == 5
