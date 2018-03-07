30572e71dfd3111de4de04d3f99439713f405a7c
DavindraS <razrsharp25@gmail.com>
2018-03-07 08:37:13 -0500

    Adds some input checking a repeating loop to check other loan amounts

--- lesson_2/mortgage_calc.rb
+++ lesson_2/mortgage_calc.rb
@@ -1,12 +1,42 @@
-puts('Welcome to my Loan Calculator')
-puts('Please enter the loan amount')
-loan_amount = gets.chomp.to_f
-puts('What\'s your APR? (ex. 5, 24.5, not .05 or .245)')
-apr = gets.chomp.to_f
-interest = (apr * 0.01) / 12.0 # Monthly interest
-puts('Please enter the loan duration')
-duration = gets.chomp.to_f
+loan_amount = nil
+apr = nil
+interest = nil
+duration = nil
 
-monthly = loan_amount * (interest / (1 - (1 + interest)**-duration))
+def continue?(input)
+  input.downcase.start_with?('y') ? true : false
+end
 
-puts("Your monthly payment is $#{monthly.round(2)}")
+loop do
+  puts('Welcome to the Mortgage Calculator')
+
+  loop do
+    puts('Please enter the loan amount')
+    loan_amount = gets.chomp.to_f
+    if loan_amount < 0
+      puts('Please enter an amount greater than 0')
+      next
+    end
+    break
+  end
+
+  loop do
+    puts('What\'s your APR? (ex. 5, 24.5, not .05 or .245)')
+    apr = gets.chomp.to_f
+    if apr < 1
+      puts('Please enter the APR in the correct format (6.5 instead of .065)')
+      next
+    else
+      interest = (apr * 0.01) / 12.0 # Monthly interest
+    end
+    break
+  end
+
+  puts('Please enter the loan duration in years')
+  duration = gets.chomp.to_f * 12
+
+  monthly = loan_amount * (interest / (1 - (1 + interest)**-duration))
+  puts("Your monthly payment is $#{monthly.round(2)}")
+  puts('Would you like to check monthly payments for another loan? (yes or no)')
+  break unless continue?(gets.chomp)
+end
