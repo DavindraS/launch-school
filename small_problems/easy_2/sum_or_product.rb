fb8319f5682bb418306922bd512f7a05e198984b
DavindraS <razrsharp25@gmail.com>
2018-03-08 22:00:23 -0500

    Add some input validation and the correct outputs according to the example

--- small_problems/easy_2/sum_or_product.rb
+++ small_problems/easy_2/sum_or_product.rb
@@ -1,10 +1,29 @@
-puts 'Please enter an integer greater than 0:'
-number = gets.chomp.to_i
-puts "Enter 's' to compute the sum, 'p' to compute the product."
-operation = gets.chomp
+number = nil
+operation = nil
 
-if operation == 's'
-  puts (1..number).inject(:+)
-elsif operation = 'p'
-  puts (1..number).inject(:*)
+loop do
+  loop do
+    puts 'Please enter an integer greater than 0:'
+    number = gets.chomp.to_i
+    break unless number.negative?
+    puts "That number doesn't fit the criteria"
+  end
+
+  loop do
+    puts "Enter 's' to compute the sum, 'p' to compute the product."
+    operation = gets.chomp
+
+    if operation == 's'
+      puts "The sum of the integers between 1 and #{number} is #{(1..number).inject(:+)}."
+      break
+    elsif operation == 'p'
+      puts "The product of the integers between 1 and #{number} is #{(1..number).inject(:*)}."
+      break
+    else
+      puts "Wrong input"
+    end
+  end
+
+  puts "Do you want to do another calculation?"
+  break unless gets.chomp.downcase.include?('y')
 end
