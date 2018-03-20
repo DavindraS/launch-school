75e4201ff788cd49fe66062b8e74ec0634b54a8a
DavindraS <razrsharp25@gmail.com>
2018-03-20 15:49:57 -0400

    Add my solution to Searching 101 exersice

---
+++ small_problems/easy_3/searching_101.rb
@@ -0,0 +1,35 @@
+=begin
+Ask the user to enter a number into every slot of a 5 length array
+Ask them to enter a last number (that we will check for)
+Check if the array contains the last number
+print out the result of the Check
+=end
+
+def translate_num(number)
+  case number
+  when 1 then '1st'
+  when 2 then '2nd'
+  when 3 then '3rd'
+  when 4 then '4th'
+  when 5 then '5th'
+  end
+end
+
+numbers = Array.new(5)
+
+def get_numbers(numbers_array)
+  numbers_array.collect!.with_index do |x, i|
+    puts "==> Please enter the #{translate_num(i+1)} number:"
+    x = gets.chomp.to_i
+  end
+end
+
+get_numbers(numbers)
+puts '==> Enter the last number:'
+search_num = gets.chomp.to_i
+
+if numbers.include?(search_num)
+  puts "The number #{search_num} appears in #{numbers}."
+else
+  puts "The number #{search_num} does not appear in #{numbers}."
+end
