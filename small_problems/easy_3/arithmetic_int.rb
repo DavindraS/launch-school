01d021198328fcbc4af32f0c953b7bda28c58ef6
DavindraS <razrsharp25@gmail.com>
2018-03-20 16:25:33 -0400

    Add rubocop fixes

--- small_problems/easy_3/arithmetic_int.rb
+++ small_problems/easy_3/arithmetic_int.rb
@@ -1,7 +1,7 @@
-puts "Enter the first number:"
-first_num = gets.chomp.to_i
-puts "Enter the secon number:"
-second_num = gets.chomp.to_i
+puts 'Enter the first number:'
+num1 = gets.chomp.to_i
+puts 'Enter the second number:'
+num2 = gets.chomp.to_i
 
 def perform_op(num_one, num_two, operation)
   case operation
@@ -10,12 +10,12 @@ def perform_op(num_one, num_two, operation)
   when '*' then num_one * num_two
   when '/' then num_one / num_two
   when '%' then num_one % num_two
-  when '**' then num_one ** num_two
+  when '**' then num_one**num_two
   end
 end
 
 op_arr = ['+', '-', '*', '/', '%', '**']
 
 op_arr.each do |op|
-  puts "==> #{first_num} #{op} #{second_num} = #{perform_op(first_num, second_num, op)}"
+  puts "==> #{num1} #{op} #{num2} = #{perform_op(num1, num2, op)}"
 end
