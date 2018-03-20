6177d7db8dcdd41625a164e5e8b0496a94f77ff0
DavindraS <razrsharp25@gmail.com>
2018-03-20 16:16:23 -0400

    Add my solution to Arithmetic Integer exercise

---
+++ small_problems/easy_3/arithmetic_int.rb
@@ -0,0 +1,21 @@
+puts "Enter the first number:"
+first_num = gets.chomp.to_i
+puts "Enter the secon number:"
+second_num = gets.chomp.to_i
+
+def perform_op(num_one, num_two, operation)
+  case operation
+  when '+' then num_one + num_two
+  when '-' then num_one - num_two
+  when '*' then num_one * num_two
+  when '/' then num_one / num_two
+  when '%' then num_one % num_two
+  when '**' then num_one ** num_two
+  end
+end
+
+op_arr = ['+', '-', '*', '/', '%', '**']
+
+op_arr.each do |op|
+  puts "==> #{first_num} op #{second_num} = #{perform_op(first_num, second_num, op)}"
+end
