e38480b7071572c30f1a95265f2d6dd44b25b103
DavindraS <razrsharp25@gmail.com>
2018-03-21 23:08:21 -0400

    Add solution for Squaring an Arg exersice

--- small_problems/easy_3/squaring.rb
+++ small_problems/easy_3/squaring.rb
@@ -8,3 +8,21 @@ end
 
 puts square(5) == 25
 puts square(-8) == 64
+
+# further exploration
+def power(num, pow)
+  sum = 1
+  upto = 0
+  loop do
+    break if upto == pow
+    sum = multiply(sum, num)
+    upto += 1
+  end
+  sum
+end
+
+puts power(5, 1) == 5
+puts power(5, 2) == 25
+puts power(5, 3) == 125
+puts power(5, 4) == 625
+puts power(5, 5) == 3125
