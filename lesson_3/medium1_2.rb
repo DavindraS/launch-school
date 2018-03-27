5539c8b68b2a9e6342d8e9783b047945d35d8f84
DavindraS <razrsharp25@gmail.com>
2018-03-26 21:47:14 -0400

    Add answer to question 2

---
+++ lesson_3/medium1_2.rb
@@ -0,0 +1,4 @@
+puts "the value of 40 + 2 is " + (40 + 2)
+
+# This gives an error because we are attempting to add an integer to a string
+# which doesn't make sense. The + operator for a String works with 2 strings.
