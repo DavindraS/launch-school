53319edd4a1dc1bab5f4b1c87bfe19f3dd19e669
DavindraS <razrsharp25@gmail.com>
2018-03-26 22:05:58 -0400

    Add my answer to question 4

---
+++ lesson_3/medium1_4.rb
@@ -0,0 +1,3 @@
+Using << will add the new element onto the existing buffer object. On the
+other hand, using + will return a third array resulting from the concatenation
+of the the input_array with the new element.
