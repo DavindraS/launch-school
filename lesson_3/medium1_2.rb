5e32f45f563682a68b3d09dd495c51f83ceef9c7
DavindraS <razrsharp25@gmail.com>
2018-03-26 21:49:01 -0400

    Add potential solutions to original problem

--- lesson_3/medium1_2.rb
+++ lesson_3/medium1_2.rb
@@ -1,4 +1,9 @@
-puts "the value of 40 + 2 is " + (40 + 2)
+# puts "the value of 40 + 2 is " + (40 + 2)
 
 # This gives an error because we are attempting to add an integer to a string
 # which doesn't make sense. The + operator for a String works with 2 strings.
+
+# potential solution 1
+puts "The value of 40 + 2 is #{(40 + 2)}"
+# potential solution 2
+puts "The value of 40 + 2 is " + (40 + 2).to_s
