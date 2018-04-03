66b941de6aaf4263756bd4f5507497cbf62c67a5
DavindraS <razrsharp25@gmail.com>
2018-04-02 22:18:12 -0400

    Add example methods

--- lesson_3/medium1_10.rb
+++ lesson_3/medium1_10.rb
@@ -1,3 +1,11 @@
 bar(foo) will be calling bar('yes') since foo() always returns 'yes'. So
 param then equals 'yes', and when the ternary statement is run, 'yes' != 'no'
 so 'no' will be returned.
+
+def foo(param = "no")
+  "yes"
+end
+
+def bar(param = "no")
+  param == "no" ? "yes" : "no"
+end
