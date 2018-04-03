dd32aa2a495e4414658bc390303c09be53a66854
DavindraS <razrsharp25@gmail.com>
2018-04-02 22:08:57 -0400

    Add hash, method, and method call

--- lesson_3/medium1_8.rb
+++ lesson_3/medium1_8.rb
@@ -1,2 +1,19 @@
 The musters hash was modified because thats what is being passed into the
-method. 
+method.
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+def mess_with_demographics(demo_hash)
+  demo_hash.values.each do |family_member|
+    family_member["age"] += 42
+    family_member["gender"] = "other"
+  end
+end
+
+mess_with_demographics(munsters)
