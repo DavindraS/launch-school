a7dcf256353b5e8713386e2a4004e360e19ac167
DavindraS <razrsharp25@gmail.com>
2018-04-04 21:07:52 -0400

    Add answer for question 3b

--- lesson_3/hard1_3.rb
+++ lesson_3/hard1_3.rb
@@ -16,4 +16,23 @@ puts "two is: #{two}" # this should be 'two'
 puts "three is: #{three}" # this should be 'three'
 
 # In mess_with_vars, the method variables point to the value of the parameters
-# not the actual variables...so nothing gets changed outside of the method. 
+# not the actual variables...so nothing gets changed outside of the method.
+
+# b
+def mess_with_vars(one, two, three)
+  one = "two"
+  two = "three"
+  three = "one"
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}" # 'one'
+puts "two is: #{two}" # 'two'
+puts "three is: #{three}" # 'three'
+
+# Same as above. The variables in mess_with_vars do not affect the outside vars.
