a12bd2c7b40965d2e7134a9b6e2d9efc3a75ab52
DavindraS <razrsharp25@gmail.com>
2018-04-04 20:38:19 -0400

    Add my answer to question 1

---
+++ lesson_3/hard1_1.rb
@@ -0,0 +1,9 @@
+if false
+  greeting = “hello world”
+end
+
+greeting
+
+
+I expect nothing to happen. The solution states that when you initialize
+a variable in an if block, it is initialized to nil. 
--- lesson_3/medium2_5.rb
+++ lesson_3/medium2_5.rb
@@ -1,3 +1,4 @@
+
 def color_valid(color)
   color == "blue" || color == "green"
 end
