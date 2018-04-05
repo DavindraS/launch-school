5815b8a149f3731ca81771a60d9dd75014bb85d3
DavindraS <razrsharp25@gmail.com>
2018-04-04 21:10:14 -0400

    Add answer for question 3c

--- lesson_3/hard1_3.rb
+++ lesson_3/hard1_3.rb
@@ -36,3 +36,22 @@ puts "two is: #{two}" # 'two'
 puts "three is: #{three}" # 'three'
 
 # Same as above. The variables in mess_with_vars do not affect the outside vars.
+
+# c
+def mess_with_vars(one, two, three)
+  one.gsub!("one","two")
+  two.gsub!("two","three")
+  three.gsub!("three","one")
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}" # 'two'
+puts "two is: #{two}" # 'three'
+puts "three is: #{three}" # 'one'
+
+# gsub! modifies a variable in place, so it affects the outside variables.
