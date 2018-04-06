338f2d81877be90ba1e215ea0bf1dbfb4e649ead
DavindraS <razrsharp25@gmail.com>
2018-04-05 20:33:44 -0400

    Add solution for Multiples of 3 and 5 exercise

---
+++ small_problems/easy_4/mult35.rb
@@ -0,0 +1,12 @@
+def multisum(num)
+  sum = 0
+  1.upto(num) do |num|
+    sum += num if num % 3 == 0 || num % 5 == 0
+  end
+  sum 
+end
+
+puts multisum(3) == 3
+puts multisum(5) == 8
+puts multisum(10) == 33
+puts multisum(1000) == 234168
