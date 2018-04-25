47b57f1805e286594ace09985b3a0ecb1b3379b9
DavindraS <razrsharp25@gmail.com>
2018-04-24 21:51:01 -0400

    Add my solution for Running Totals exercise

---
+++ small_problems/easy_4/running_totals.rb
@@ -0,0 +1,14 @@
+def running_total(arr)
+  arr.map!.with_index do |x, i|
+    if i == 0
+      x
+    else
+      x += arr.fetch(i-1)
+    end
+  end 
+end
+
+puts running_total([2, 5, 13]) == [2, 7, 20]
+puts running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
+puts running_total([3]) == [3]
+puts running_total([]) == []
