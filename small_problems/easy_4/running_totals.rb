eb574cff47c7d3963d2e596036ef92dbc028cfc1
DavindraS <razrsharp25@gmail.com>
2018-04-24 21:51:51 -0400

    Add the actual solution as a comment

--- small_problems/easy_4/running_totals.rb
+++ small_problems/easy_4/running_totals.rb
@@ -5,10 +5,17 @@ def running_total(arr)
     else
       x += arr.fetch(i-1)
     end
-  end 
+  end
 end
 
 puts running_total([2, 5, 13]) == [2, 7, 20]
 puts running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
 puts running_total([3]) == [3]
 puts running_total([]) == []
+
+=begin actual solution
+def running_total(array)
+  sum = 0
+  array.map { |value| sum += value }
+end
+=end
