29720dac32359e1d68c0760673ac4b2b62407fa5
DavindraS <razrsharp25@gmail.com>
2018-04-24 21:02:18 -0400

    Add my answer for practice problem 6

---
+++ lesson_4/pp6.rb
@@ -0,0 +1,8 @@
+Practice Problem 6
+
+['ant', 'bear', 'caterpillar'].pop.size
+
+My Answer:
+Array.pop will return the last element in the array. Size will then get the
+size of that popped element, which in this case is 'caterpillar' whose size
+is 11, which is what is returned. 
