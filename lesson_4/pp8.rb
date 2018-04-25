13a7409afa44a2b42779725dfcc80f0ada68f07f
DavindraS <razrsharp25@gmail.com>
2018-04-24 21:18:32 -0400

    Add my answer for practice problem 8

---
+++ lesson_4/pp8.rb
@@ -0,0 +1,12 @@
+Practice Problem 8
+
+arr = [1, 2, 3, 4, 5]
+arr.take(2)
+
+My Answer:
+I did not know what take does, so I looked it up in the docs under Array.take.
+It says that it returns the first n elements, in this case the first 2 elements
+  or [1, 2].
+
+The docs don't say whether it is destructive or not, so to make sure, I just
+tried it out in irb. 
