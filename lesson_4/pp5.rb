af27e0821713ddc2907a3e352290fa5be7f64c77
DavindraS <razrsharp25@gmail.com>
2018-04-24 20:57:07 -0400

    Add my answer for practice problem 5

---
+++ lesson_4/pp5.rb
@@ -0,0 +1,8 @@
+Practice Problem 5
+
+hash = { a: 'ant', b: 'bear' }
+hash.shift
+
+My Answer:
+This will return the array [a, 'ant'] and hash will leave with just {b: 'bear'}.
+I found this out looking in the Hash documentation under shift. 
