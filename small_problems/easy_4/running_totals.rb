5ef2263c3c7bddd195cfde7999090730597e3892
DavindraS <razrsharp25@gmail.com>
2018-04-30 08:57:24 -0400

    Adds solution using Enumerable#inject

--- small_problems/easy_4/running_totals.rb
+++ small_problems/easy_4/running_totals.rb
@@ -1,13 +1,23 @@
-# Further exploration
+# Further exploration 1
 def running_total(arr)
   sum = 0
   arr.each_with_object([]) {|value, new_arr| new_arr << sum += value}
 end
 
-puts running_total([2, 5, 13]) == [2, 7, 20]
-puts running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
-puts running_total([3]) == [3]
-puts running_total([]) == []
+# Further exploration 2
+def running_total2(arr)
+  sum = 0
+  new_arr = []
+  new_arr = arr.inject([]) do |memo, n|
+    sum += n
+    memo << sum
+  end
+end
+
+puts running_total2([2, 5, 13]) == [2, 7, 20]
+puts running_total2([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
+puts running_total2([3]) == [3]
+puts running_total2([]) == []
 
 =begin my original solution
 def running_total(arr)
