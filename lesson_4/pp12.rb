2ef0260ea5a565d86dd101ad0f9a6fee636a7766
DavindraS <razrsharp25@gmail.com>
2018-04-30 20:56:15 -0400

    Add solution for practice problem 2

---
+++ lesson_4/pp12.rb
@@ -0,0 +1,6 @@
+# add up all the ages
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+
+sum_of_ages = ages.values.inject(:+)
+
+p sum_of_ages
