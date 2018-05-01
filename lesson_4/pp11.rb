d03c0190347cc3a4061e51c0cbea25188824941b
DavindraS <razrsharp25@gmail.com>
2018-04-30 20:48:49 -0400

    Add another solution to the problem based on official solution

--- lesson_4/pp11.rb
+++ lesson_4/pp11.rb
@@ -1,8 +1,15 @@
 # Turn this array into hash {name: position}
 flintstones = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "BamBam"]
 
-flint_hash = flintstones.each_with_object({}) do |name, hash|
+flint_hash_1 = flintstones.each_with_object({}) do |name, hash|
   hash[name] = flintstones.index(name)
 end
 
-p flint_hash
+# another way based on official answer
+flint_hash_2 = {}
+flintstones.each_with_index do |name, index|
+  flint_hash_2[name] = index
+end
+
+p flint_hash_1
+p flint_hash_2
