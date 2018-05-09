880aa160c95c2506a3a890f70f7b22b43bc24ae5
DavindraS <razrsharp25@gmail.com>
2018-05-08 22:54:59 -0400

    Add code for the further exploration

--- small_problems/easy_5/ascii_value.rb
+++ small_problems/easy_5/ascii_value.rb
@@ -4,7 +4,7 @@ def ascii_value(string)
   string.each_char do |chr|
     sum += chr.ord
   end
-  
+
   sum
 end
 
@@ -12,3 +12,6 @@ puts ascii_value('Four score') == 984
 puts ascii_value('Launch School') == 1251
 puts ascii_value('a') == 97
 puts ascii_value('') == 0
+
+# further exploration
+p 'a'.ord.chr == 'a' # find the mystery method
