ae881440e643333df62f95d91097b7bf7ccaa56d
DavindraS <razrsharp25@gmail.com>
2018-05-08 21:57:32 -0400

    Add my solution to the exercise

--- small_problems/easy_4/num_to_str.rb
+++ small_problems/easy_4/num_to_str.rb
@@ -1,6 +1,15 @@
 # converting a number to a string
+DIGITS = {
+  0 => '0', 1 => '1', 2 => '2', 3 => '3', 4 => '4',
+  5 => '5', 6 => '6', 7 => '7', 8 => '8', 9 => '9'
+}
+
 def integer_to_string(number)
-  result = number.digits.reverse.join
+  str = ''
+  number.digits.reverse.each do |num|
+    str << DIGITS[num]
+  end
+  str
 end
 
 puts integer_to_string(4321) == '4321'
