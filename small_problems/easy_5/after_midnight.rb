867ef6e666286766595bc445c768f43f884889fd
DavindraS <razrsharp25@gmail.com>
2018-05-08 22:59:38 -0400

    Add intial code for solution

---
+++ small_problems/easy_5/after_midnight.rb
@@ -0,0 +1,12 @@
+# take time as minute and return as 24 hour time (hh:mm)
+def time_of_day(min)
+
+end
+
+puts time_of_day(0) == "00:00"
+puts time_of_day(-3) == "23:57"
+puts time_of_day(35) == "00:35"
+puts time_of_day(-1437) == "00:03"
+puts time_of_day(3000) == "02:00"
+puts time_of_day(800) == "13:20"
+puts time_of_day(-4231) == "01:29"
