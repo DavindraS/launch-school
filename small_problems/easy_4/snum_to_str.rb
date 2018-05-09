494c18777b2184e7e21a281627b374bec44f5f93
DavindraS <razrsharp25@gmail.com>
2018-05-08 22:03:53 -0400

    Using int_to_str from previous exercise

---
+++ small_problems/easy_4/snum_to_str.rb
@@ -0,0 +1,18 @@
+DIGITS = {
+  0 => '0', 1 => '1', 2 => '2', 3 => '3', 4 => '4',
+  5 => '5', 6 => '6', 7 => '7', 8 => '8', 9 => '9'
+}
+
+def integer_to_string(number)
+  str = ''
+  number.digits.reverse.each do |num|
+    str << DIGITS[num]
+  end
+  str
+end
+
+
+
+puts signed_integer_to_string(4321) == '+4321'
+puts signed_integer_to_string(-123) == '-123'
+puts signed_integer_to_string(0) == '0'
