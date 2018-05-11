f09be63d79aa955d3a08c429ea8f1b2156599af0
DavindraS <razrsharp25@gmail.com>
2018-05-10 21:52:45 -0400

    Add solution to After Midnight exercise

--- small_problems/easy_5/after_midnight.rb
+++ small_problems/easy_5/after_midnight.rb
@@ -1,6 +1,51 @@
 # take time as minute and return as 24 hour time (hh:mm)
-def time_of_day(min)
+MINUTES_PER_DAY = 1440
+def time_of_day(minutes)
+=begin
+  time = ''
+  hour = 0
+  minutes_left = 0
+  backwards_minutes = 0
 
+  if minutes < 1440 && minutes >= 0
+    hour = minutes / 60
+    minutes_left = minutes % 60
+  elsif minutes > 1440
+    hour = minutes / 1440
+    minutes_left = minutes % 60
+  elsif minutes < 0 && minutes > -1440
+    backwards_minutes = 1440 + minutes
+    hour = backwards_minutes / 60
+    minutes_left = backwards_minutes % 60
+  else
+    backwards_minutes = 1440 + minutes
+    hour = backwards_minutes / 1440
+    minutes_left = backwards_minutes % 60
+  end
+
+  if hour < 10
+    hour_string = "0#{hour}"
+  else
+    hour_string = "#{hour}"
+  end
+
+  if minutes_left < 10
+    minute_string = "0#{minutes_left}"
+  else
+    minute_string = "#{minutes_left}"
+  end
+
+  time = "#{hour_string}:#{minute_string}"
+  p time
+=end
+
+  mins_after_midnight = minutes % MINUTES_PER_DAY
+  hour, minute = mins_after_midnight.divmod(60)
+
+  hour = "0#{hour}" if hour < 10
+  minute = "0#{minute}" if minute < 10
+
+  p "#{hour}:#{minute}"
 end
 
 puts time_of_day(0) == "00:00"
