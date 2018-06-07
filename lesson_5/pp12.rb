ef06844549cb478311c050b8d32b418d2bbae801
DavindraS <razrsharp25@gmail.com>
2018-06-06 21:35:32 -0400

    Set up and explation of practice problem 12

---
+++ lesson_5/pp12.rb
@@ -0,0 +1,3 @@
+# Without using array.to_h, convert this array into a hash
+# with the first item the key and the second item the value
+arr = [[:a, 1], ['b', 'two'], ['sea', {c: 3}], [{a: 1, b: 2, c: 3, d: 4}, 'D']]
