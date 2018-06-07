f7af4712df412a1020f1d123275ae50e6539c44a
DavindraS <razrsharp25@gmail.com>
2018-06-06 22:35:59 -0400

    Add problem and set up

---
+++ lesson_5/pp14.rb
@@ -0,0 +1,12 @@
+# Return an array with the colors of the fruit and the size of the vegies
+# Size should be uppercase and color should be capitalized
+
+hsh = {
+  'grape' => {type: 'fruit', colors: ['red', 'green'], size: 'small'},
+  'carrot' => {type: 'vegetable', colors: ['orange'], size: 'medium'},
+  'apple' => {type: 'fruit', colors: ['red', 'green'], size: 'medium'},
+  'apricot' => {type: 'fruit', colors: ['orange'], size: 'medium'},
+  'marrow' => {type: 'vegetable', colors: ['green'], size: 'large'},
+}
+
+solution = [["Red", "Green"], "MEDIUM", ["Red", "Green"], ["Orange"], "LARGE"]
