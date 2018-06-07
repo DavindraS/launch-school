d6280707d88c7017f3a285016c429f5cf76b201d
DavindraS <razrsharp25@gmail.com>
2018-06-06 21:31:24 -0400

    Add solution for practice problem 11

--- lesson_5/pp11.rb
+++ lesson_5/pp11.rb
@@ -1,3 +1,13 @@
 # return a new array of identical structure, but only with
 # numbers that are multiples of 3
 arr = [[2], [3, 5, 7], [9], [11, 13, 15]]
+
+mod = arr.map do |subarr|
+  subarr.select do |item|
+    if item % 3 == 0
+      item
+    end
+  end
+end
+
+p mod
