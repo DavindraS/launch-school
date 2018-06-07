a9b425fba3da29487c89f0948788ecca89a59c14
DavindraS <razrsharp25@gmail.com>
2018-06-06 22:02:56 -0400

    Add set up and explanation of problem

---
+++ lesson_5/pp13.rb
@@ -0,0 +1,7 @@
+# Return a new array with the same subarrays, but ordered by the odd
+# numbers they contain
+
+arr = [[1, 6, 7], [1, 4, 9], [1, 8, 3]]
+solution = [[1, 8, 3], [1, 6, 7], [1, 4, 9]]
+
+p arr == solution
