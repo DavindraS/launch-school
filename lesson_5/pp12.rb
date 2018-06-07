58a15e6904b328bcd0ca791da613f63b521ce7b9
DavindraS <razrsharp25@gmail.com>
2018-06-06 21:49:06 -0400

    Add solution for practice problem 12

--- lesson_5/pp12.rb
+++ lesson_5/pp12.rb
@@ -1,3 +1,10 @@
-# Without using array.to_h, convert this array into a hash
+# Without using array.to_h, create a hash
 # with the first item the key and the second item the value
 arr = [[:a, 1], ['b', 'two'], ['sea', {c: 3}], [{a: 1, b: 2, c: 3, d: 4}, 'D']]
+
+mod = arr.map do |subarr|
+  Hash[subarr[0], subarr[1]]
+end
+
+p mod
+p arr
