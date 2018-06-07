0bd00bd735d7f53804ec09da54673cf68d233a83
DavindraS <razrsharp25@gmail.com>
2018-06-06 21:22:14 -0400

    Adds corrected answer with explanation of error in original solution

--- lesson_5/pp10.rb
+++ lesson_5/pp10.rb
@@ -1,9 +1,21 @@
 arr = [{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}]
-
+=begin
+This solution is wrong because this modifies arr. The problem
+specifies that we are not to modify the original array.
 arr.map do |hsh|
   hsh.map do |k, v|
     hsh[k] = v + 1
   end
 end
+=end
 
+new = arr.map do |hsh|
+  incremented_hash = {}
+  hsh.each do |k, v|
+    incremented_hash[k] = v + 1
+  end
+  incremented_hash
+end
+
+p new # I added this to verify the increment happened
 p arr
