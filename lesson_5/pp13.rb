579c9e58dc76bf5f339d7751a19699ea2c2b198b
DavindraS <razrsharp25@gmail.com>
2018-06-06 22:12:30 -0400

    Add initial solution for practice problem 13

--- lesson_5/pp13.rb
+++ lesson_5/pp13.rb
@@ -4,4 +4,12 @@
 arr = [[1, 6, 7], [1, 4, 9], [1, 8, 3]]
 solution = [[1, 8, 3], [1, 6, 7], [1, 4, 9]]
 
-p arr == solution
+modified = arr.sort_by do |subarr|
+  acc = 0
+  subarr.each do |item|
+    acc += item if item.odd?
+  end
+  acc
+end
+
+p modified == solution
