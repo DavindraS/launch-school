f13855e68720e84f085bed054a19ef33d7933188
DavindraS <razrsharp25@gmail.com>
2018-06-07 21:45:46 -0400

    Moved around some bits of code for easier readability

--- small_problems/easy_5/after_midnight.rb
+++ small_problems/easy_5/after_midnight.rb
@@ -1,4 +1,18 @@
 # take time as minute and return as 24 hour time (hh:mm)
+MINUTES_PER_HOUR = 60
+def time_of_day(minute_change)
+  hour, minute = minute_change.divmod(MINUTES_PER_HOUR)
+  format("%02d:%02d", hour % 24, minute)
+end
+
+puts time_of_day(0) == "00:00"
+puts time_of_day(-3) == "23:57"
+puts time_of_day(35) == "00:35"
+puts time_of_day(-1437) == "00:03"
+puts time_of_day(3000) == "02:00"
+puts time_of_day(800) == "13:20"
+puts time_of_day(-4231) == "01:29"
+
 =begin
 MINUTES_PER_DAY = 1440
 def time_of_day(minutes)
@@ -57,25 +71,5 @@ puts time_of_day(35) == "00:35"
 puts time_of_day(-1437) == "00:03"
 puts time_of_day(3000) == "02:00"
 puts time_of_day(800) == "13:20"
-puts time_of_day(-4231) == "01:29"\
-
-require 'time'
-# Further exploration - do the same thing, but can use Date and Time classes
-def time_of_day(minutes)
-  p Time.new(minutes)
-end
-
-=end
-MINUTES_PER_HOUR = 60
-def time_of_day(minute_change)
-  hour, minute = minute_change.divmod(MINUTES_PER_HOUR)
-  format("%02d:%02d", hour % 24, minute)
-end
-
-puts time_of_day(0) == "00:00"
-puts time_of_day(-3) == "23:57"
-puts time_of_day(35) == "00:35"
-puts time_of_day(-1437) == "00:03"
-puts time_of_day(3000) == "02:00"
-puts time_of_day(800) == "13:20"
 puts time_of_day(-4231) == "01:29"
+=end
