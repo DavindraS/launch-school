7f1c1d92714f6cfd72114a234acb90a0bf7c9b1c
DavindraS <razrsharp25@gmail.com>
2018-06-07 23:20:33 -0400

    Add solution for letter swap exercise

--- small_problems/easy_5/letter_swap.rb
+++ small_problems/easy_5/letter_swap.rb
@@ -1,6 +1,15 @@
 # Taking a string, return a string where the 1st and last letters are switched
 def swap(str)
-
+  strarr = str.split(' ') # 'hello'
+  letterarr = strarr.map {|word| word.chars} # ['h','e','l','l','o']
+  letterarr.map! do |letters| # 'oellh'
+    first = letters.first
+    last = letters.last
+    letters[0] = last
+    letters[letters.length-1] = first
+    letters.join
+  end
+  letterarr.join(' ')
 end
 
 p swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
