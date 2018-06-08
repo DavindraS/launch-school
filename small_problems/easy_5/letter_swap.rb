834a6ba506bd98156e544f66bd56190f06e6cc35
DavindraS <razrsharp25@gmail.com>
2018-06-07 22:22:05 -0400

    Set up and explanation of exercise

---
+++ small_problems/easy_5/letter_swap.rb
@@ -0,0 +1,8 @@
+# Taking a string, return a string where the 1st and last letters are switched
+def swap(str)
+
+end
+
+p swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
+p swap('Abcde') == 'ebcdA'
+p swap('a') == 'a'
