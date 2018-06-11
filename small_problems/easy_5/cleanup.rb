b629f35f7bb51772d4dce5799728d09f154ba85a
DavindraS <razrsharp25@gmail.com>
2018-06-11 09:33:55 -0400

    Add my first implemenation of a solution to the exercise

--- small_problems/easy_5/cleanup.rb
+++ small_problems/easy_5/cleanup.rb
@@ -1,6 +1,21 @@
 # Given a string, strip out all non alphabetic characters
 def cleanup(str)
-  
+  alphabet = ('a'..'z').to_a
+  new_str = str.chars.map.with_index do |letter, index|
+    if !alphabet.include?(letter) # if you're not an alphabetic character
+      if alphabet.include?(str.chars[index+1]) # if the next char is alphabetic
+        ' '
+      elsif index == str.chars.size-1 # if we're at the end
+        ' '
+      else
+        str.chars[index] = nil # delete the character
+      end
+    else
+      letter # return the character
+    end
+  end.join # join the chars array
+
+  new_str # return the new string 
 end
 
 p cleanup("---what's my +*& line?") == ' what s my line '
