6098bc6e6cac20ed45a63c359317fcf2077f8608
DavindraS <razrsharp25@gmail.com>
2018-06-12 22:18:58 -0400

    Add official solution

--- small_problems/easy_5/letter_count.rb
+++ small_problems/easy_5/letter_count.rb
@@ -27,3 +27,13 @@ puts word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
 puts word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
 puts word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
 puts word_sizes('') == {}
+
+=begin official solution
+def word_sizes(words_string)
+  counts = Hash.new(0)
+  words_string.split.each do |word|
+    counts[word.size] += 1
+  end
+  counts
+end
+=end
