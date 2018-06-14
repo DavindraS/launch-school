14faddc0247398ad35be2ab83a17d95585a3049d
DavindraS <razrsharp25@gmail.com>
2018-06-13 20:42:35 -0400

    Add solution to the exercise

---
+++ small_problems/easy_5/alpha_numbers.rb
@@ -0,0 +1,14 @@
+# input: array of ints,output: array of ints sorted by english word for each  num
+
+ALPHA_NUM = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven',
+  'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen',
+  'sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+def alphabetic_number_sort(num_arr)
+  num_arr.sort {|a, b| ALPHA_NUM[a] <=> ALPHA_NUM[b]}
+end
+
+p alphabetic_number_sort((0..19).to_a) == [
+  8, 18, 11, 15, 5, 4, 14, 9, 19, 1, 7, 17,
+  6, 16, 10, 13, 3, 12, 2, 0
+]
