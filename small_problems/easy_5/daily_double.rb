faf2b4c9236b94b308c2bab883ce759d30224c57
DavindraS <razrsharp25@gmail.com>
2018-06-13 20:56:40 -0400

    Inital set up and explanation of exercise

---
+++ small_problems/easy_5/daily_double.rb
@@ -0,0 +1,10 @@
+# take a string and remove any consecutive duplicate characters
+def crunch(double_str)
+
+end 
+
+crunch('ddaaiillyy ddoouubbllee') == 'daily double'
+crunch('4444abcabccba') == '4abcabcba'
+crunch('ggggggggggggggg') == 'g'
+crunch('a') == 'a'
+crunch('') == ''
